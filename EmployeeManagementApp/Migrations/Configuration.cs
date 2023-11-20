d4cd900887ccb5bae442e0bb9dbe093fc3389cef
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-20 19:47:54 +0530

    Add files via upload

---
+++ EmployeeManagementApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EmployeeManagementApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EmployeeManagementApp.DAL.EmployeeDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EmployeeManagementApp.DAL.EmployeeDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
