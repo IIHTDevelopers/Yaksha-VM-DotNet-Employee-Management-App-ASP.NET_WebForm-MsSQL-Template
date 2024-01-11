5a0418a7beb4e5c98dc4aadde138042a662e6c91
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:08:41 +0530

    Add files via upload

---
+++ EmployeeManagementApp/DAL/Interfaces/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagementApp.Model;
+
+namespace EmployeeManagementApp.DAL.Interfaces
+{
+    public interface IEmployeeService
+    {
+        string GetAll();
+        string Add();
+        string Update();
+        string Delete();
+        EmployeeModel GetById(string id);
+    }
+}
---
+++ EmployeeManagementApp/DAL/Services/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using EmployeeManagementApp.DAL.Interfaces;
+
+namespace EmployeeManagementApp.DAL.Services
+{
+    public class EmployeeRepository : Interfaces.IEmployeeRepository
+    {
+        private EmployeeDbContext _context;
+
+        public EmployeeRepository(EmployeeDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.EmployeeModel GetById(string id)
+        {
+            return _context.EmployeeModels.FirstOrDefault(t => t.Id == int.Parse(id));
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        { 
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
