facd811209d3a19ef73397de709699a8032b0dd6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-12 15:55:00 +0530

    Add files via upload

---
+++ EmployeeManagementApp/DAL/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using EmployeeManagementApp.Model;
+
+namespace EmployeeManagementApp.DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Model.EmployeeModel> EmployeeModels { get; set; }
+
+        public EmployeeDbContext() : base("ConnStr")
+        {
+        }
+    }
+}
---
+++ EmployeeManagementApp/DAL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeManagementApp.Model;
+
+namespace EmployeeManagementApp.DAL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        string GetAll();
+        string Add();
+        string Update();
+        string Delete();
+
+        EmployeeModel GetById(string id);
+
+    }
+}
